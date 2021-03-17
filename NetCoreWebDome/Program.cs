5a9351df20ab3dbf950b12d74ffeb503ff480141
小白 <2806933146@qq.com>
2021-03-17 17:26:17 +0800

    提交说明

--- NetCoreWebDome/Program.cs
+++ NetCoreWebDome/Program.cs
@@ -12,13 +12,19 @@ namespace NetCoreWebDome
 {
     public class Program
     {
+        /// <summary>
+        /// Main方法，程序的入口方法
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)  //调用下面的方法，返回一个IWebHostBuilder对象(Web主机（承载和运行Web应用程序的常规工具）配置实例)
+                .Build()                // 用上面返回的IWebHostBuilder对象创建一个IWebHost(Web主机（承载和运行Web应用程序的常规工具）实例)
+                .Run();                 // 运行Web应用程序一直监听http请求
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args)  // WebHostBuilder新实例
+                .UseStartup<Startup>();         // 为WebHost指定了启动类（新实例.UseStartup<启动类>()）
     }
 }
